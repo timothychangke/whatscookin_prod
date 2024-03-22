a863c45f74e400f09e9268f30b4284756f7c00c6
josiahyong <josiahyong123@gmail.com>
2024-03-22 15:15:24 +0800

    button implementation to fetch api details

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
-import './App.css';
+// import './App.css';
+// import Axiosrecipecall from './components/Axiosrecipecall.jsx';
+import FetchForm from './components/FetchForm.js';
 
 function App() {
-  return <div className="App"></div>;
+  return(
+  <div className="App">
+    <h1>HI</h1>
+    <FetchForm/>
+  </div>
+  );
 }
 
 export default App;
